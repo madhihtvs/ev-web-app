bb3791535af8340156af074ab08b31154e0df315
madhihtvs <106566123+madhihtvs@users.noreply.github.com>
2022-07-18 07:11:55 +0800

    Update backend.py

--- preprocessing/backend.py
+++ preprocessing/backend.py
@@ -3,6 +3,7 @@ import requests
 
 
 def get_coordinates(location):
+    """Getting coordinates from address"""
     url = "https://api.geoapify.com/v1/geocode/search"
 
     params = dict(text=location, apiKey="af361475cd624479ab85363a1893eab1")
@@ -13,6 +14,7 @@ def get_coordinates(location):
     return data
 
 def get_address(lat, lon):
+    """Getting address from coordinates to be added to marker"""
     url = "https://api.geoapify.com/v1/geocode/reverse"
     
     params = dict(lat=lat,
@@ -30,10 +32,10 @@ def get_address(lat, lon):
     
 
 def get_POI(lat, lon, radius):
-
+    """Getting Points of Interest when specified latitude, longitude and radius"""
     url = "https://api.geoapify.com/v2/places"
     params = dict(
-        categories='entertainment,catering,commercial,leisure,tourism',
+        categories='entertainment,catering,commercial,leisure,tourism', # places searched to fall under these categories 
         filter=f'circle:{lon},{lat},{radius}',
         bias=f'proximity:{lon},{lat}',
         limit=20,
@@ -60,6 +62,7 @@ def get_POI(lat, lon, radius):
 
 
 def get_Hotel(lat, lon):
+    """Getting places of accommmodation given a latitude and longitude with a specific radius of 5000km"""
     url = "https://api.geoapify.com/v2/places"
     params = dict(
         categories='accommodation',
@@ -76,12 +79,12 @@ def get_Hotel(lat, lon):
     POI = []
     for i in range(len(features)):
         try:
-            name = features[i]["properties"]["name"]
+            name = features[i]["properties"]["name"] #some of the data start with name
         except:
-            name = features[i]["properties"]["street"]
-        category = features[i]["properties"]["categories"][0]
-        lat = features[i]["geometry"]["coordinates"][::-1][0]
-        lon = features[i]["geometry"]["coordinates"][::-1][1]
+            name = features[i]["properties"]["street"] #others start with street
+        category = features[i]["properties"]["categories"][0] #category of place
+        lat = features[i]["geometry"]["coordinates"][::-1][0] # lat of place
+        lon = features[i]["geometry"]["coordinates"][::-1][1] # lon of place
         POI.append([name, category, lat, lon])
 
     df = pd.DataFrame(POI, columns = ["Name","Category","Latitude", "Longitude"])
@@ -91,6 +94,7 @@ def get_Hotel(lat, lon):
 
 
 def get_route(orig_lat, orig_lon, dest_lat, dest_lon):
+    """Get polyline path between origin and destination. Also return total distance and total time together with list of coordinates"""
     url = "https://api.geoapify.com/v1/routing"
     querystring = {
         "waypoints": f"{orig_lat},{orig_lon}|{dest_lat},{dest_lon}",
@@ -112,6 +116,7 @@ def get_route(orig_lat, orig_lon, dest_lat, dest_lon):
     return lst, distance, time
 
 def get_route_short(orig_lat, orig_lon, dest_lat, dest_lon):
+    """Get polyline path between origin and destination which is shorter than the default. Also return total distance and total time together with list of coordinates"""
     url = "https://api.geoapify.com/v1/routing"
     querystring = {
         "waypoints": f"{orig_lat},{orig_lon}|{dest_lat},{dest_lon}",
@@ -133,6 +138,7 @@ def get_route_short(orig_lat, orig_lon, dest_lat, dest_lon):
     return lst, distance, time
 
 def get_route_less_maneuvers(orig_lat, orig_lon, dest_lat, dest_lon):
+    """Get polyline path between origin and destination which has less maneuvers. Also return total distance and total time together with list of coordinates. However this is not used. Only 2 routes are shown"""
     url = "https://api.geoapify.com/v1/routing"
     querystring = {
         "waypoints": f"{orig_lat},{orig_lon}|{dest_lat},{dest_lon}",
@@ -154,20 +160,11 @@ def get_route_less_maneuvers(orig_lat, orig_lon, dest_lat, dest_lon):
     return lst, distance, time
 
 
-def get_route_geojson(orig_lat, orig_lon, dest_lat, dest_lon):
-    url = "https://api.geoapify.com/v1/routing"
-    querystring = {
-        "waypoints": f"{orig_lat},{orig_lon}|{dest_lat},{dest_lon}",
-        "mode": "motorcycle",
-        "apiKey": "af361475cd624479ab85363a1893eab1",
-    }
-    response = requests.request("GET", url, params=querystring)
-    data = response.json()
-    return data
 
 
 
 def get_route_many(*points):
+    """Get polyline path between multiple points (No Limit). Also return total distance and total time together with list of coordinates"""
     string = ""
     for i in points[0]:
         lat = i[0]
@@ -200,6 +197,7 @@ def get_route_many(*points):
 
 
 def get_route_many_short(*points):
+    """Get polyline path between multiple points (No Limit) which is shorter than balanced route. Also return total distance and total time together with list of coordinates"""
     string = ""
     for i in points[0]:
         lat = i[0]
